6e3b72ab9685a513c0796fee5ab698880c36ae05
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-26 15:01:10 +0530

    Add files via upload

---
+++ Swaping.java
@@ -0,0 +1,16 @@
+public class Swaping
+{
+	public static void main(String[] args) 
+	{
+	    int a=10, b=20,t;
+	  
+		System.out.println("before swaping"+a);
+	  System.out.println("before swaping"+b);
+	   t=a;
+	   a=b;
+	   b=t;
+	  System.out.println("after swaping"+a);
+	 System.out.println("after swaping"+b);
+	 
+}
+}
